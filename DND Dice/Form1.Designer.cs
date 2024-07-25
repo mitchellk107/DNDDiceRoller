9f9e47ffa0b28ff7509cd32cce6c6e81e09d52ae
Mitchell Kelly <mitchellkelly107@gmail.com>
2024-07-25 01:46:12 -0700

    C1

---
+++ DND Dice/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace DND_Dice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.D4Button = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.DiceDisplay = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.SuspendLayout();
+            // 
+            // D4Button
+            // 
+            this.D4Button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.D4Button.Location = new System.Drawing.Point(79, 70);
+            this.D4Button.Name = "D4Button";
+            this.D4Button.Size = new System.Drawing.Size(122, 39);
+            this.D4Button.TabIndex = 0;
+            this.D4Button.Text = "D4";
+            this.D4Button.UseVisualStyleBackColor = false;
+            this.D4Button.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.button2.Location = new System.Drawing.Point(79, 115);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(122, 39);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "D6";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.button3.Location = new System.Drawing.Point(79, 160);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(122, 39);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "D8";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Violet;
+            this.button4.Location = new System.Drawing.Point(79, 205);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(122, 39);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "D10";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.button5.Location = new System.Drawing.Point(79, 250);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(122, 39);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "D12";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button6.Location = new System.Drawing.Point(79, 295);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(122, 39);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "D20";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button12
+            // 
+            this.button12.BackColor = System.Drawing.Color.Maroon;
+            this.button12.Location = new System.Drawing.Point(207, 160);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(122, 39);
+            this.button12.TabIndex = 11;
+            this.button12.Text = "ROLL";
+            this.button12.UseVisualStyleBackColor = false;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // DiceDisplay
+            // 
+            this.DiceDisplay.AutoSize = true;
+            this.DiceDisplay.BackColor = System.Drawing.Color.White;
+            this.DiceDisplay.Font = new System.Drawing.Font("Verdana", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DiceDisplay.Location = new System.Drawing.Point(610, 215);
+            this.DiceDisplay.Name = "DiceDisplay";
+            this.DiceDisplay.Size = new System.Drawing.Size(40, 38);
+            this.DiceDisplay.TabIndex = 14;
+            this.DiceDisplay.Text = "0";
+            this.DiceDisplay.Click += new System.EventHandler(this.DiceDisplay_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(79, 51);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(122, 302);
+            this.groupBox1.TabIndex = 15;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Dice";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::DND_Dice.Properties.Resources.D4BG;
+            this.ClientSize = new System.Drawing.Size(751, 398);
+            this.Controls.Add(this.DiceDisplay);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.D4Button);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button D4Button;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Label DiceDisplay;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ DND Dice/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DND_Dice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //public variables
+        bool rolling = false;
+
+        // Dice Guide: 1 = d4, 2 = d6, 3 = d8, 4 = d10, 5 = d12, 6 = d20
+        int diceType = 1;
+        int diceNum = 0;
+
+        //initializes the sound player and the random number class
+        SoundPlayer diceRoll = new SoundPlayer("C:\\Dev\\c# class\\DND Dice\\DND Dice\\Assets\\dice_roll.wav");
+        Random random = new Random();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //D4 Button
+            // 1 = d4, 2 = d6, 3 = d8, 4 = d10, 5 = d12, 6 = d20
+            diceType = 1;
+            DiceLogic();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //D6 Dice
+            // 1 = d4, 2 = d6, 3 = d8, 4 = d10, 5 = d12, 6 = d20
+            diceType = 2;
+            DiceLogic();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //D10 Dice
+            // 1 = d4, 2 = d6, 3 = d8, 4 = d10, 5 = d12, 6 = d20
+            diceType = 4;
+            DiceLogic();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //D12 Dice
+            // 1 = d4, 2 = d6, 3 = d8, 4 = d10, 5 = d12, 6 = d20
+            diceType = 5;
+            DiceLogic();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //D20 Dice
+            // 1 = d4, 2 = d6, 3 = d8, 4 = d10, 5 = d12, 6 = d20
+            diceType = 6;
+            DiceLogic();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //D8 Button
+            // 1 = d4, 2 = d6, 3 = d8, 4 = d10, 5 = d12, 6 = d20
+            diceType = 3;
+            DiceLogic();
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            //Roll Button
+            diceRoll.Play();
+            rolling = true;
+            DiceLogic();
+            
+        }
+        
+        public void DiceLogic()
+        {
+            //Determines what will happen after the diceType iniger is changed
+            if (diceType == 1) 
+            { 
+                diceNum = random.Next(1, 5);
+                BackgroundImage = Properties.Resources.D4BG;
+            }
+            if (diceType == 2) 
+            { 
+                diceNum = random.Next(1, 7);
+                BackgroundImage = Properties.Resources.D6BG;
+            }
+            if (diceType == 3) 
+            {
+                BackgroundImage = Properties.Resources.D8BG;
+                diceNum = random.Next(1, 9); 
+            }
+            if (diceType == 4) {
+
+                BackgroundImage = Properties.Resources.D10BG;
+                diceNum = random.Next(1, 11);
+            }
+            if (diceType == 5) {
+
+                BackgroundImage = Properties.Resources.D12BG;
+                diceNum = random.Next(1, 13); 
+            }
+            if (diceType == 6) {
+
+                BackgroundImage = Properties.Resources.D20BG;
+                diceNum = random.Next(1, 21); 
+            }
+
+            // Rolling logic This will happen when rolled
+            if (rolling == true)
+            {
+                DiceDisplay.Text = diceNum.ToString();
+                rolling = false;
+            }
+
+            
+            
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DiceDisplay_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
